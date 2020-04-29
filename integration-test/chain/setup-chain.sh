e74617f370e8bc583827be38216c39f007c982ec
Shuhei Hiya <syuhei176@gmail.com>
2020-04-29 10:52:41 +0900

    allow unlimited contract size

--- integration-test/chain/setup-chain.sh
+++ integration-test/chain/setup-chain.sh
@@ -2,7 +2,7 @@
 set -m
 
 # start chain with db
-ganache-cli --mnemonic "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat" --db /HOME/db &
+ganache-cli --mnemonic "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat" --db /HOME/db -l 0x8091b7 --allowUnlimitedContractSize &
 GANACHE_PID=$!
 
 # deploy contracts to chain
